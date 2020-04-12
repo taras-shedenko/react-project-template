220927d73ae34edf56cecbee0760e321eeefd80a
Taras Shedenko <tshedenko@grubhub.com>
2020-04-12 15:56:36 +0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+    <div>React Project Template</div>,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({ template: './src/index.html' });
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx']
+    },
+    plugins: [htmlWebpackPlugin]
+};
